e8a052e327e639b1f360722fa2192f99f2c58f59
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-10-21 00:52:09 +0530

    Added : Welcome message

---
+++ src/main/java/com/bdlz/binarytree/BinaryTree.java
@@ -0,0 +1,7 @@
+package com.bdlz.binarytree;
+
+public class BinaryTree {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Binary tree data structures program");
+    }
+}
